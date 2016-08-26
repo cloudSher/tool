5624498a5cafad4d4045eb07c0927dfe24a8fb23
zhaowei3@lashou-inc.com <zhaowei3@lashou-inc.com>
2016-08-26 17:51:39 +0800

    添加mysql connection test method

--- src/main/java/com/sher/tool/mysql/Connection.java
+++ src/main/java/com/sher/tool/mysql/Connection.java
@@ -1,7 +1,9 @@
 package com.sher.tool.mysql;
 
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 /**
  * Created by Administrator on 2016/8/24.
@@ -11,11 +13,12 @@ public class Connection {
     private static String url = "jdbc:mysql://localhost:3306/";
     private static String user = "root";
     private static String pwd="root";
+    private static java.sql.Connection connection;
 
     static {
         try {
             Class.forName("com.mysql.jdbc.Driver");
-            DriverManager.getConnection(url,user,pwd);
+            java.sql.Connection connection = DriverManager.getConnection(url, user, pwd);
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
         } catch (SQLException e) {
@@ -23,7 +26,21 @@ public class Connection {
         }
     }
 
-//    public void
+    public java.sql.Connection getConnection(){
+        return connection;
+    }
+
+    public void execute(String sql) throws SQLException {
+        //建立sql陈述对象
+        Statement statement = connection.createStatement();
+        statement.execute(sql);
+    }
+
+    public void prepareStatement(String sql) throws SQLException {
+        PreparedStatement prepareStatement = connection.prepareStatement(sql);
+        prepareStatement.execute(sql);
+
+    }
 
 
 
--- src/main/java/com/sher/tool/mysql/Datasource.java
+++ src/main/java/com/sher/tool/mysql/Datasource.java
@@ -2,6 +2,17 @@ package com.sher.tool.mysql;
 
 /**
  * Created by Administrator on 2016/8/24.
+ *
+ *  数据源
  */
 public class Datasource {
+
+
+//    private
+
+    public Datasource(){
+
+    }
+
+
 }
