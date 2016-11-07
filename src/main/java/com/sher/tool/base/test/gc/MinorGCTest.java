aec896bcba44961beff550d4bd2fa24f920409a7
zhaowei3@lashou-inc.com <zhaowei3@lashou-inc.com>
2016-11-07 19:27:33 +0800

    gc test

--- src/main/java/com/sher/tool/base/test/gc/MinorGCTest.java
+++ src/main/java/com/sher/tool/base/test/gc/MinorGCTest.java
@@ -1,22 +1,51 @@
 package com.sher.tool.base.test.gc;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Administrator on 2016/10/11.
- *
+ *  reference http://www.cnblogs.com/yang-hao/p/5939487.html
  *  gc test
  */
 public class MinorGCTest {
 
     /***
-     *
+     *  -Xms10m -Xmx10m -verbose:gc -server
      */
     public static void minorGC(){
+//        Object[] buffer = new Object[10 * 1000 * 1000];     //java heap space out of memory exception
+        int num = 0;
+        Object obj;
+        Runtime.getRuntime().addShutdownHook(new Thread(){
+            public void run(){
+                Thread thread = Thread.currentThread();
+                while(thread.isInterrupted()){
+                    thread.interrupt();
+                }
+            }
+        });
+        List list = new ArrayList<>();
+        while(true){
+            obj = new Object();
+            list.add(obj);
+//            try {
+//                Thread.sleep(50);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+            System.out.println(++num);
+        }
+    }
+
+    public static void yonggc(){
 
     }
 
 
 
-    public static void main(String args[]){
 
+    public static void main(String args[]){
+        minorGC();
     }
 }
