9fb5d7776c985322f6877a0b7630e169be87c0f8
zhaowei3@lashou-inc.com <zhaowei3@lashou-inc.com>
2016-11-09 19:21:35 +0800

    add zk lock,leaderElection

--- server/src/main/java/com/sher/server/zookeeper/Lock.java
+++ server/src/main/java/com/sher/server/zookeeper/Lock.java
@@ -1,26 +1,92 @@
 package com.sher.server.zookeeper;
 
-import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Created by Administrator on 2016/10/21.
  */
-public class Lock {
+public class Lock extends MainClient{
 
-    private ZooKeeper zk;
+    private String myLock;
 
-    public Lock(){
+    public Lock(String connection,String root){
+        super(connection);
+        this.root = root;
         try {
-            zk = ZookeeperClientDemo.createZK("",0);
-        } catch (IOException e) {
+            Stat stat = zk.exists(root, false);
+            if(stat == null){
+                zk.create(root,new byte[0], ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.PERSISTENT);
+            }
+        } catch (KeeperException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 
+    public void check() throws KeeperException, InterruptedException {
+        myLock = zk.create(root + "/lock_", new byte[0], ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL_SEQUENTIAL);
+        System.out.println("my lock is " + myLock);
+        getLock();
+    }
+
+    public void getLock() throws KeeperException, InterruptedException {
+        List<String> children = zk.getChildren(root, false);
+        String[] array = children.toArray(new String[children.size()]);
+        Arrays.sort(array);
+        if(myLock.equals(root+"/"+array[0])){
+            doAction();
+        }else{
+            doWaitForLock(array[0]);
+        }
+    }
+
+    private void doWaitForLock(String lock) {
+        try {
+            Stat stat = zk.exists(root + "/" + lock, true);
+            if(stat == null){
+                getLock();
+            }else{
+                synchronized (mutex){
+                    mutex.wait();
+                }
+            }
+        } catch (KeeperException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public void process(WatchedEvent event) {
+        if(event.getType() == Event.EventType.NodeDeleted){
+            System.out.println("process event action");
+            super.process(event);
+            doAction();     // 一定会是顺序执行吗？
+        }
+    }
+
+    private void doAction() {
+        System.out.println("do some thing ...");
+    }
+
 
     public static void main(String args[]){
-
+        Lock lock = new Lock("localhost:2181","/locks");
+        try {
+            lock.check();
+            Thread.sleep(10000);
+        } catch (KeeperException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
--- server/src/main/java/com/sher/server/zookeeper/MainServer.java
+++ server/src/main/java/com/sher/server/zookeeper/MainServer.java
@@ -1,12 +1,62 @@
 package com.sher.server.zookeeper;
 
 import org.apache.zookeeper.server.ZooKeeperServerMain;
+import org.apache.zookeeper.server.quorum.QuorumPeerConfig;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 
 /**
  * Created by Administrator on 2016/11/8.
  */
 public class MainServer extends ZooKeeperServerMain{
 
+    private static String DEFAULT_PATH = "D:\\zookeeper-3.5.2-alpha\\conf\\";
+    private static String DEFAULT_CONFIG = "zoo.cfg";
 
+    public static void main(String args[]){
+        MainServer server = new MainServer();
+        String config = server.default_config().toString();
+        try {
+            server.initializeAndRun(new String[]{config});
+        } catch (QuorumPeerConfig.ConfigException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public File default_config(){
+        return new File(DEFAULT_PATH + DEFAULT_CONFIG);
+    }
+
+    /***
+     * clientPort=2181
+       dataDir=e://zookeeper/data
+       syncLimit=5
+       initLimit=10
+       tickTime=2000
+     * @return
+     * @throws IOException
+     */
+    public File tempConfigFile() throws IOException {
+        File config = new File(tempFile(),DEFAULT_CONFIG);
+        FileWriter writer = new FileWriter(config);
+        writer.write("clientPort=2181\n");
+        File data = new File(tempFile(),"data");
+        if(!data.exists()){
+            data.mkdir();
+        }
+        writer.write("dataDir=" + data.toString() + "\n");
+        writer.write("syncLimit=5\n");
+        writer.write("initLimit=10\n");
+        writer.write("tickTime=2000\n");
+        return config;
+    }
+
+    public File tempFile(){
+        return new File(System.getProperty("user.path"));
+    }
 
 }
