c0f102a74c25decb194e2d444b4d43e5b74e109d
zhaowei3@lashou-inc.com <zhaowei3@lashou-inc.com>
2016-11-25 18:23:38 +0800

    modify socket demo

--- src/main/java/com/sher/tool/base/test/basicType/StringTest.java
+++ src/main/java/com/sher/tool/base/test/basicType/StringTest.java
@@ -9,8 +9,20 @@ import org.junit.Test;
 public class StringTest {
 
 
-    @Test
+
     public void test(){
         Assert.assertEquals(new String(new char[]{'1','4','7'}).intern() == "147",true);
     }
+
+
+    public static void main(String args[]){
+        String  s = "aa"+"bb";
+        String s1 = "aabb";
+        String a = "aa";
+        String b = "bb";
+        String c = a + b;
+        System.out.println(s == s1);        //true;
+        System.out.println(c == s1);        //false;
+    }
+
 }
--- src/main/java/com/sher/tool/net/SocketDemo.java
+++ src/main/java/com/sher/tool/net/SocketDemo.java
@@ -1,31 +1,18 @@
 package com.sher.tool.net;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.*;
 import java.nio.channels.ServerSocketChannel;
 import java.nio.channels.SocketChannel;
+import java.util.concurrent.CountDownLatch;
 
 /**
  * Created by cloudsher on 2016/5/17.
  */
 public class SocketDemo {
-
-    public static void main(String args[]){
-        try {
-            InetAddress inetAddress = InetAddress.getByName("10.1.34.24");
-            int port = 9300;
-            DatagramSocket socket = new DatagramSocket();
-            socket.connect(inetAddress,port);
-            if(socket.isConnected()){
-                System.out.println("========");
-            }
-            socket.close();
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        } catch (SocketException e) {
-            e.printStackTrace();
-        }
-    }
 
     /***
      *  udp socket impl
@@ -53,10 +40,10 @@ public class SocketDemo {
      * @throws IOException
      */
     public static Socket socketToTcp(String ip,int port,int time) throws IOException {
-        Socket socket = new Socket();
-        socket.connect(new InetSocketAddress(InetAddress.getByName(ip),port),time);
-        if(!socket.isConnected())
-            throw new NullPointerException("socket must not be null");
+        Socket socket = new Socket(ip,port);
+//        socket.connect(new InetSocketAddress(InetAddress.getLocalHost(),port),time);
+//        if(!socket.isConnected())
+//            throw new NullPointerException("socket must not be null");
         return socket;
     }
 
@@ -66,7 +53,81 @@ public class SocketDemo {
         private SocketChannel channel;
         private ServerSocketChannel serverSocket;
 
+    }
 
+    public static void serverSocket(int port) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(port);
+        int count = 0;
+        while(true){
+            Socket accept = serverSocket.accept();
+            count++;
+            InputStream stream = accept.getInputStream();
+            printStream(stream);
+            OutputStream out = accept.getOutputStream();
+            out.write(count);
+            out.flush();
+        }
+    }
+
+    public static void printStream(InputStream in) throws IOException {
+        if(in != null){
+            ByteArrayOutputStream out = new ByteArrayOutputStream();
+            byte[] buffer = new byte[1024];
+            int len;
+            while((len = in.read()) != -1){
+                out.write(buffer,0,len);
+            }
+            //print
+            System.out.println(new String(out.toByteArray()));
+        }
+    }
+
+    private static CountDownLatch latch = new CountDownLatch(1);
+
+    public static void main(String args[]) throws InterruptedException {
+        new Server().run(8080);
+        latch.await();
+//        new Client().run(1000,"10.1.34.24",8080);
 
     }
+
+    static class Server{
+
+        public void run(int port){
+            new Thread(()->{
+                try {
+                    serverSocket(port);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+            latch.countDown();
+        }
+    }
+
+    static class Client{
+        private int threadNum;
+        public void run(int threadNum,String ip,int port){
+            this.threadNum = threadNum;
+            for(int i = 0 ; i < threadNum; i++){
+                new Thread(()->{
+                    try {
+                        Socket socket = socketToTcp(ip, port, 1000);
+                        InputStream stream = socket.getInputStream();
+                        printStream(stream);
+                        OutputStream out = socket.getOutputStream();
+                        out.write("hello".getBytes());
+                        out.flush();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }).start();
+            }
+        }
+    }
+
+
+
+
+
 }
